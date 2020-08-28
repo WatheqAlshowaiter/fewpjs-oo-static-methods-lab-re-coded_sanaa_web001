a2eceb140e02ca7eebde8df217ea41b61d85eba6
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-28 23:27:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ class Formatter {
   //add static methods here
   // 1.  Write a method `static capitalize` that takes in a string and capitalizes the
   //   first letter.
-  static capitalize( string ) {
-    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   // 2.  Write a method `static sanitize` that takes in a string and removes all
@@ -18,13 +18,33 @@ class Formatter {
   //     always capitalizes the first word.
 
   static titleize(srtingOfTitle) {
-    Array.from(srtingOfTitle); // convert to an array
+    // let firstWord = srtingOfTitle.split(' ');
+    let stopwords = ["a", "an", "but", "of", "and", "for", "at", "by", "from"];
 
+    let firstWord = srtingOfTitle.replace(/ .*/, "");
+    firstWord = this.capitalize(firstWord);
+
+    let otherWords = srtingOfTitle.split(" ");
+    otherWords.shift();
+    console.log(otherWords);
+
+    let newArry = [];
+    newArry.push(firstWord);
+
+    for (let i = 0; i < otherWords.length; i++) {
+      if (!stopwords.includes(otherWords[i])) {
+        newArry.push(this.capitalize(otherWords[i]));
+      }
+    }
+    return newArry;
+
+    // console.log(firstWord)
     // capitalize first word
-    let firstWord = Formatter.capitalize(srtingOfTitle[0]);
-    let otherWords = srtingOfTitle.filter((word) =>
-      word.includes(["a", "an", "but", "of", "and", "for", "at", "by", "from"])
-    );
-    return firstWord + otherWords;
+    // let firstWord = Formatter.capitalize(srtingOfTitle[0]);
+    // let otherWords = srtingOfTitle.filter((word) =>
+    // word.includes(["a", "an", "but", "of", "and", "for", "at", "by", "from"])
+    // );
+    // return firstWord + otherWords;
   }
 }
+Formatter.titleize("in the night kitchen");
