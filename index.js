4fed003ed322c83597271fcbcf8d621a58c125e6
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-28 23:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ class Formatter {
   //add static methods here
   // 1.  Write a method `static capitalize` that takes in a string and capitalizes the
   //   first letter.
-  static capitalizes(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
   }
+
   // 2.  Write a method `static sanitize` that takes in a string and removes all
   //     non-alphanumeric characters except for dashes, single quotes and spaces.
-  // static 
+  // static
+  static sanitize(string) {
+    return string.replace(/[^-,'A-Za-z0-9]+/g, "");
+  }
+
   // 3.  Write a method `static titleize` that takes in a string and capitalizes all
   //     words in a sentence except the, a, an, but, of, and, for, at, by, and from; and
   //     always capitalizes the first word.
+
+  static titleize(srtingOfTitle) {
+    Array.from(srtingOfTitle); // convert to an array
+
+    ["a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    // capitalize first word
+    let firstWord = Formatter.capitalize(srtingOfTitle[0]);
+    let otherWords = srtingOfTitle.filter((word) =>
+      word.includes(["a", "an", "but", "of", "and", "for", "at", "by", "from"])
+    );
+    return firstWord + otherWords;
+  }
 }
